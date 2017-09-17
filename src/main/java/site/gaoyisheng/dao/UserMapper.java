d1156f4fa9a955e101272a23bb913c7a50bd5227
Gao Yisheng <1160139211@qq.com>
2017-09-17 21:37:33 +0800

    MyBatis auto mapping code

---
+++ src/main/java/site/gaoyisheng/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package site.gaoyisheng.dao;
+
+import site.gaoyisheng.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/site/gaoyisheng/pojo/User.java
@@ -0,0 +1,43 @@
+package site.gaoyisheng.pojo;
+
+public class User {
+    private String id;
+
+    private Object username;
+
+    private Object password;
+
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Object getUsername() {
+        return username;
+    }
+
+    public void setUsername(Object username) {
+        this.username = username;
+    }
+
+    public Object getPassword() {
+        return password;
+    }
+
+    public void setPassword(Object password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
